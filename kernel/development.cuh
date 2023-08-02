6540d7e769315ae716591bcd46af78cb7523424c
ngaylinn <108184212+ngaylinn@users.noreply.github.com>
2023-08-02 04:36:02 -0700

    Update development.cuh

--- kernel/development.cuh
+++ kernel/development.cuh
@@ -1,6 +1,3 @@
-#ifndef __DEVELOPMENT_H__
-#define __DEVELOPMENT_H__
-
 /*
  * Primitive functions for building a phenotype.
  *
@@ -10,12 +7,18 @@
  * all the different functions that the Operations in interpreter.cuh can run.
  */
 
+#ifndef __DEVELOPMENT_H__
+#define __DEVELOPMENT_H__
+
 #include "environment.h"
 #include "phenotype_program.h"
 
 namespace epigenetic_gol_kernel {
 
-// Calculate one cell value in an organism phenotype.
+// Calculate one cell value in an organism phenotype. Calling this function will
+// modify the value of Cell, which occupies position (row, col) within the GOL
+// board. The new value for Cell is computed by interpreting program and running
+// it against the data found in genotype.
 __device__ void make_phenotype(
         const PhenotypeProgram& program, const Genotype& genotype,
         const int& row, const int& col, Cell& cell);
